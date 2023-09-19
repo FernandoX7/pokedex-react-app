ad69eed870d1d7c319e92bdcaf2aa5e2c5cf63f0
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2023-09-19 13:17:04 -0500

    Update the search bar to dispatch an action on search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,28 @@
 import classes from './SearchBar.module.css';
+import { useDispatch } from "react-redux";
+import { ChangeEvent, useMemo } from "react";
+import { debounce } from "lodash";
+import { fetchPokemonByName } from "../../store/actions/pokemon-actions";
 
 const SearchBar = () => {
+  const debounceTimeInMs = 300;
+  const dispatch = useDispatch();
+
+  const dispatchData = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(fetchPokemonByName(event.target.value));
+  };
+
+  const searchInputHandler = useMemo(() => {
+    return debounce(dispatchData, debounceTimeInMs);
+  }, []);
+
   return (
     <div className={classes.SearchBar}>
       <input
         className={classes.SearchInput}
         placeholder="Search for a Pokémon"
         type="text"
+        onChange={searchInputHandler}
       />
     </div>
   );
