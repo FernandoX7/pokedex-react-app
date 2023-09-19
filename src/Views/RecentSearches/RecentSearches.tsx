522d37b792c8e6547a65d3e81d77df9547e0d84e
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2023-09-18 21:05:35 -0500

    Add RecentSearches view

---
+++ src/Views/RecentSearches/RecentSearches.tsx
@@ -0,0 +1,15 @@
+import classes from './RecentSearches.module.css';
+
+const RecentSearches = () => {
+  return (
+    <div className={classes.RecentSearches}>
+      <div>Test</div>
+      <div>Test</div>
+      <div>Test</div>
+      <div>Test</div>
+      <div>Test</div>
+    </div>
+  );
+};
+
+export default RecentSearches;
