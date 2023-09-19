e4edd6dfba93275e2b9619d012bff9bfa467950c
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2023-09-18 21:28:28 -0500

    Add PokemonDetailsView (wip)

---
+++ src/Views/PokemonDetailsView/PokemonDetailsView.tsx
@@ -0,0 +1,11 @@
+import classes from './PokemonDetailsView.module.css';
+
+const PokemonDetailsView = () => {
+  return (
+    <div className={classes.PokemonDetailsView}>
+      Pokemon details
+    </div>
+  );
+};
+
+export default PokemonDetailsView;
